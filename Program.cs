8a49c0ee24020fd38e6f5f4cf4427bf9d5f3501b
Filip Kosmala <fkosmala@mpk.krakow.pl>
2021-11-19 14:55:33 +0100

    Add localization support

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Localization;
+using System.Globalization;
+using Microsoft.Extensions.Options;
 using DutyAndConductorManager.Web;
 using MudBlazor.Services;
 using DutyAndConductorManager.Web.Services;
@@ -15,6 +19,7 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IHttpService, HttpService>();
 builder.Services.AddScoped<ILocalStorageService, LocalStorageService>();
 builder.Services.AddScoped<IThemeLibrary, ThemeLibrary>();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 builder.Services.AddMudServices();
 
 var host = builder.Build();
